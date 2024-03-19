49bc37571e3f85679e8fc059f1ebff48136b083c
Aravind <agavinikadi@gmail.com>
2024-03-19 19:53:17 +0530

    Your commit message here

--- client/signup/src/navbar.jsx
+++ client/signup/src/navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './navbar.css';
+import {Login,Home, Signup} from './Signup.jsx';
+import { Link } from 'react-router-dom';
 const Navbar = ({buttonpress}) => {
   function click(name) {
     buttonpress(name);
@@ -9,12 +11,18 @@ const Navbar = ({buttonpress}) => {
     <nav className='Navbar'>
       <div className='left'>
         <button className='navele' id='logo'>SFSS</button>
+        <Link to='/'>
         <button className='navele' onClick={()=>click('HOME')}>HOME</button>
+        </Link>
         <button className='navele'onClick={()=>click('RESTAURANTS')}>RESTAURANTS</button>
       </div>
       <div className='right'>
+        <Link to='/signin'>
         <button className='navele' onClick={()=>click('SIGNIN')}>SIGNIN</button>
+        </Link>
+        <Link to='/signup'>
         <button className='navele' onClick={()=>click('REGISTER')}>REGISTER</button>
+        </Link>
       </div>
     </nav>
   );
--- client/signup/src/page1.jsx
+++ client/signup/src/page1.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import {Login,Home, Signup} from './Signup.jsx';
+import { Login, Home, Signup } from './Signup.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Navbar from './navbar.jsx';
 
 function App() {
-  const [navbardata, setnavbardata] = useState('Home');
+  const [navbardata, setnavbardata] = useState('HOME');
 
   function getdatafromnavbar(data) {
     setnavbardata(data);
@@ -12,13 +13,16 @@ function App() {
 
   return (
     <>
-      <Navbar buttonpress={getdatafromnavbar} />
-      <div>
-        {navbardata === 'SIGNIN' && <Login></Login>}
-        {navbardata === 'HOME' && <Home />}
-        {navbardata === 'REGISTER' && <Signup />}
-      </div>
+      <BrowserRouter>
+        <Navbar buttonpress={getdatafromnavbar} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 export default App;
+
